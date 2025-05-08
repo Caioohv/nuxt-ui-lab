acab6985aba91d2283c58775ac0b665884fa6ab6
Caio Vieira <caioviier@gmail.com>
2025-05-08 20:01:21 -0300

    chore default styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="flex flex-col ">
-    <UButton color="primary">Hello world</UButton>
-  
-    <span class="text-primary">Primary</span>
-    <span class="text-secondary">Secondary</span>
-    <span class="text-success">Success</span>
-    <span class="text-info">Info</span>
-    <span class="text-warning">Warning</span>
-    <span class="text-error">Error</span>
+    
   </div>
 </template>
